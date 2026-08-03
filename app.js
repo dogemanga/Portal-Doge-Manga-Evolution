027a02089d05e57ff867e5199c50923e4a6eb9ca
Ok$dogemangaofficial <dogemanga687@gmail.com>
2026-08-03 15:34:20 -0300

    Implement token registration and display functions
    Added functions to register and display tokens in local storage.

--- app.js
+++ app.js
@@ -1,1 +1,114 @@
+let moedas =
+JSON.parse(localStorage.getItem("moedas")) || [];
 
+
+
+function cadastrarToken(){
+
+
+let moeda={
+
+nome:nome.value,
+
+simbolo:simbolo.value,
+
+rede:rede.value,
+
+contrato:contrato.value,
+
+logo:logo.value,
+
+site:site.value,
+
+twitter:twitter.value,
+
+telegram:telegram.value,
+
+descricao:descricao.value
+
+
+};
+
+
+moedas.push(moeda);
+
+
+localStorage.setItem(
+"moedas",
+JSON.stringify(moedas)
+);
+
+
+
+alert("Token cadastrado com sucesso!");
+
+mostrarMoedas();
+
+
+}
+
+
+
+function mostrarMoedas(){
+
+
+let area=document.getElementById("moedas");
+
+
+area.innerHTML="";
+
+
+moedas.forEach((m)=>{
+
+
+area.innerHTML += `
+
+<div class="moeda">
+
+
+<img class="logo" src="${m.logo}">
+
+
+<h2>${m.nome}
+(${m.simbolo})</h2>
+
+
+<p>
+Rede: ${m.rede}
+</p>
+
+
+<p>
+Contrato:
+${m.contrato}
+</p>
+
+
+<p>
+${m.descricao}
+</p>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+}
+
+
+
+function mostrarCadastro(){
+
+document.getElementById("cadastro")
+.style.display="block";
+
+}
+
+
+
+mostrarMoedas();
